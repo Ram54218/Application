c4d274d0091b1098b055704922728a2c591dff0a
Ram54218 <rameshram2294@gmail.com>
2024-01-16 23:22:57 +0530

    bacic counter apdsadp with cubit and bloc using flutterbloc

---
+++ Flutter/flutter_cubit/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_cubit/bloc/counter_bloc.dart';
+import 'package:flutter_cubit/cubit/counter_cubit/counter_cubit.dart';
+import 'package:flutter_cubit/pages/bloc_page.dart';
+import 'package:flutter_cubit/pages/cubit_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => CounterCubit()),
+          BlocProvider(create: (_) => CounterBloc())
+        ],
+        child: const MaterialApp(
+          // home: CubitPage()
+          home: BlocPage(),
+        ));
+  }
+}
