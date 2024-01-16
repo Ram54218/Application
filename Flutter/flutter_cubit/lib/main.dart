c00c59d27a15e504d624f775df2e4ebb90ceff84
Ram54218 <rameshram2294@gmail.com>
2024-01-16 23:25:05 +0530

    bacic counter apdsadp with cubit and bloc using flutterbloc

--- Flutter/flutter_cubit/lib/main.dart
+++ Flutter/flutter_cubit/lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
         ],
         child: const MaterialApp(
           // home: CubitPage()
-          home: BlocPage(),
+          home: BlocPage(),//
         ));
   }
 }
